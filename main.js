d0def88bf527e281565ec5e549243358729d33c3
Lorenz Bacon <150405033+Renztgm@users.noreply.github.com>
2026-02-13 23:11:19 +0800

    third Commit

--- main.js
+++ main.js
@@ -5,11 +5,37 @@ onload = () => {
   }, 1000);
 
   const message = document.querySelector(".valentine-message");
+  const yesButton = document.querySelector(".valentine-btn--yes");
+  const noButton = document.querySelector(".valentine-btn--no");
+  const actions = document.querySelector(".valentine-actions");
+  const gallery = document.querySelector(".valentine-gallery");
   const messageDelayMs = 8000;
+  const noReturnDelayMs = 1800;
 
   if (message) {
     setTimeout(() => {
       message.classList.add("is-visible");
     }, messageDelayMs);
+  }
+
+  if (yesButton && gallery && actions) {
+    yesButton.addEventListener("click", () => {
+      gallery.classList.add("is-visible");
+      actions.classList.add("is-hidden");
+    });
+  }
+
+  if (noButton) {
+    noButton.addEventListener("click", () => {
+      noButton.classList.add("is-shrinking");
+
+      setTimeout(() => {
+        noButton.classList.add("is-hidden");
+      }, 400);
+
+      setTimeout(() => {
+        noButton.classList.remove("is-hidden", "is-shrinking");
+      }, noReturnDelayMs);
+    });
   }
 };
