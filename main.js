0723fd55e3846474e9403a8b7280e5334369ee26
Lorenz Bacon <150405033+Renztgm@users.noreply.github.com>
2026-02-13 23:03:17 +0800

    Second Commit

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+
+  const message = document.querySelector(".valentine-message");
+  const messageDelayMs = 8000;
+
+  if (message) {
+    setTimeout(() => {
+      message.classList.add("is-visible");
+    }, messageDelayMs);
+  }
+};
